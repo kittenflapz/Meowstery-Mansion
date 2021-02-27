eca6a924701fd4fdfa3f4efd2591f527e884446a
kittenflapz <catherine.symonds.thompson@gmail.com>
2021-02-27 12:50:06 -0500

    ui fixes

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -52,6 +52,15 @@ public class GameManager : MonoBehaviour
         InitializeCageColors();
     }
 
+    private void Awake()
+    {
+
+        // fix input fields in webgl build
+#if !UNITY_EDITOR && UNITY_WEBGL
+            WebGLInput.captureAllKeyboardInput = true;
+#endif
+    }
+
     // Update is called once per frame
     void Update()
     {
